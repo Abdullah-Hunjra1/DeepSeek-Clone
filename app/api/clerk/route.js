eca866b5a0571c86b78f6de0b65dbb2add1f7c3a
Abdullah-Hunjra1 <abdullahhunjra111@gmail.com>
2025-10-09 23:17:04 +0500

    Frontend Completed, Backend Started

---
+++ app/api/clerk/route.js
@@ -0,0 +1,52 @@
+import { Webhook } from "svix";
+import connectDB from "@/config/db";
+import User from "@/models/User";
+import _default from "next/dist/build/templates/pages";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+
+export default async function POST(req) {
+    const wh = new Webhook(process.env.SIGNING_SECRET)
+    const headerPayLoad = await headers()
+    const svixHeaders = {
+        "svix-id": headerPayLoad.get("svix-id"),
+        "svix-signature": headerPayLoad.get("svix-signature"),
+    }
+
+    // Get the Payload and verify it
+
+    const payload = await req.json()
+    const body = JSON.stringify(payload)
+    const { data, type } = wh.verify(body, svixHeaders)
+
+    //Prepare the user data to be saved in the database
+
+    const userData = {
+        _id: data._id,
+        email: data.email_addresses[0].email_addresses,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+    }
+
+    await connectDB()
+
+    switch (type) {
+        case 'user.created':
+            await User.create(userData)
+            break;
+
+        case 'user.updated':
+            await User.findByIdAndUpdate(data.id, userData)
+            break;
+
+        case 'user.deleted':
+            await User.findByIdAndDelete(data.id, userData)
+            break;
+
+        default:
+            break;
+    }
+
+    return NextRequest.json({ message: "Event Recieved" })
+}
---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose || { conn: null, promise: null }
+
+export default async fuction connectDB() {
+    if (cached.conn) return cached.conn
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => mongoose)
+    }
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        console.log("Error Connecting to mongodb", error);
+    }
+    return cached.conn
+}
